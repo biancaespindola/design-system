7ba64bb6ed6138f5e34b4733c74254a9d3560a9c
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2022-10-27 21:45:53 -0300

    criando variação default input

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,15 +1,16 @@
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
-import { ReactNode } from 'react';
+import { InputHTMLAttributes, ReactNode } from 'react';
 
-export interface TextInputProps {}
+export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export function TextInput({}: TextInputProps) {
+export function TextInput(props: TextInputProps) {
 	return (
 		<input
 			className={clsx(
 				' py-4 px-3 rounded bg-gray-800 w-full text-gray-100 text-xs placeholder:text-gray-400 '
 			)}
+			{...props}
 		/>
 	);
 }
